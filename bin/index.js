c2b4657b0954362405980f40487d53e64a68f8cf
pablo <74920898+apausa@users.noreply.github.com>
2026-04-16 11:39:37 +0200

    Lint files

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@
 
 // Command line options
 import yargs from "yargs";
-import {hideBin} from "yargs/helpers";
+import { hideBin } from "yargs/helpers";
 
 // Three
 // import {Blob} from 'node:buffer';
@@ -10,34 +10,39 @@ import {hideBin} from "yargs/helpers";
 // import { GLTFExporter } from './GLTFExporter.js';
 
 // GLTF exporter
-import { convertGeometry } from '../lib/phoenixExport.js';
+import { convertGeometry } from "../lib/phoenixExport.js";
 
 // Node
 import * as fs from "fs";
 import * as path from "path";
 
-
 const options = yargs(hideBin(process.argv))
-    .usage("Usage: [-h] [-n <object-name>] [-o <output-file>] [-c <config-file>] <input-file>")
-    .positional("input-file", {describe: "Input ROOT file",
-                               type: "string"})
-    .option("o", {alias: "output-file",
-                  describe: "Name of output glTF file",
-                  type: "string"})
-    .option("n", {alias: "object-name",
-                  describe: "Object name",
-                  type: "string",
-                  default: "default"})
-    .option("c", {alias: "config",
-                  describe: "Configuration file for the detector",
-                  type: "string",
-                  default: "config.json"})
-    .help("h")
-    .argv;
+  .usage(
+    "Usage: [-h] [-n <object-name>] [-o <output-file>] [-c <config-file>] <input-file>",
+  )
+  .positional("input-file", { describe: "Input ROOT file", type: "string" })
+  .option("o", {
+    alias: "output-file",
+    describe: "Name of output glTF file",
+    type: "string",
+  })
+  .option("n", {
+    alias: "object-name",
+    describe: "Object name",
+    type: "string",
+    default: "default",
+  })
+  .option("c", {
+    alias: "config",
+    describe: "Configuration file for the detector",
+    type: "string",
+    default: "config.json",
+  })
+  .help("h").argv;
 
 if (!options._[0]) {
-    console.log("ERROR: Input ROOT file not provided!")
-    process.exit(1);
+  console.log("ERROR: Input ROOT file not provided!");
+  process.exit(1);
 }
 
 let configFilePath = "config.json";
@@ -52,7 +57,7 @@ const inFilePath = `${options._[0]}`;
 console.log("INFO: Reading file:");
 console.log("      " + inFilePath);
 
-let outFileName = path.parse(inFilePath).name + '.gltf';
+let outFileName = path.parse(inFilePath).name + ".gltf";
 if (options.outputFile) {
   outFileName = options.outputFile;
 }
@@ -65,9 +70,11 @@ for (var [name, entry] of Object.entries(config.subParts)) {
   entry[0] = temp;
 }
 
-convertGeometry(inFilePath,
-                outFileName,
-                config.maxLevel,
-                config.subParts,
-                config.childrenToHide,
-                options.objectName);
+convertGeometry(
+  inFilePath,
+  outFileName,
+  config.maxLevel,
+  config.subParts,
+  config.childrenToHide,
+  options.objectName,
+);
