e10555bc0141f529cbc4ac6251afa8fa289f716d
pablo <74920898+apausa@users.noreply.github.com>
2026-04-17 17:01:37 +0200

    Comment out differences with respect to legacy code

--- bin/index.js
+++ bin/index.js
@@ -42,23 +42,24 @@ import {
       entry[0] = entry[0].map((p) => new RegExp(p));
     }
 
-    cleanupGeometry(geometry.fNodes.arr[0], childrenToHide, maxLevel);
-
     // for each geometry subpart, duplicate the geometry and keep only the subpart
     console.log("INFO: Generating all scenes (one per subpart):");
 
+    // drop nodes we do not want to see at all (usually too detailed parts)
+    cleanupGeometry(geometry.fNodes.arr[0], childrenToHide, maxLevel);
+
     for (const [name, entry] of Object.entries(subParts)) {
       console.log(`      ${name}`);
-      // drop nodes we do not want to see at all (usually too detailed parts)
       // dump to gltf, using one scene per subpart
-      // set nb of degrees per face for circles approximation (default 6)
-      // here 15 means circles are polygones with 24 faces (default 60)
-      settings.GeoGradPerSegm = 360 / 24;
+      // set nb of degrees per face for circles approximation (default 24)
+      settings.GeoGradPerSegm = 360 / 24; // @fix geoCfg('GradPerSegm', 360/nFaces);
       const paths = entry[0];
       const visibility = entry[1];
-      // extract subpart of ROOT geometry
+      // @fix first reset visibility to be sure eveything is invisible
+      // @fix set_invisible_recursively(geometry.fNodes.arr[0]);
+      // make top node visible
       setVisible(geometry.fNodes.arr[0]);
-      keepOnlySubpart(geometry.fMasterVolume, paths);
+      keepOnlySubpart(geometry.fNodes.arr[0], paths);
       // convert to gltf
       const scene = new Scene();
       scene.name = name;
@@ -68,6 +69,8 @@ import {
         numfaces: 10000000,
         numnodes: 500000,
       });
+      // @fix remove from children paths that should not be there
+      // @fix cleanupChildren(children, paths, fullPath)
       scene.children.push(children);
       if (typeof visibility === "boolean") {
         scene.userData = { visible: visibility };
